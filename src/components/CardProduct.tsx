a0d78be81044185116c7e550ff6479a8f35ba007
1Salivan1 <eugenysalivan@gmail.com>
2023-09-25 21:52:49 +0300

    added some styles and added sorting on the shop page

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,3 +1,5 @@
+import "../style/CardProduct.css";
+
 interface ICardProductProps {
   id: number;
   image: string;
@@ -13,13 +15,13 @@ function CardProduct(props: ICardProductProps) {
     <div className="card">
       <div className="card-top">
         <img className="card-img" src={props.image} alt="" />
+        <h2 className="card-header">{props.name}</h2>
       </div>
       <div className="card-bottom">
-        <h2 className="card-header">{props.name}</h2>
         <p className="card-quantity">В наличии: {props.quantity}</p>
         <p className="card-price-for-one">Цена за штуку: {props.price}</p>
         <p className="card-seller">Продавец: {props.user}</p>
-        <button>Купить</button>
+        <button className="card-button">Купить</button>
       </div>
     </div>
   );
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, ChangeEvent } from "react";
 import axios from "axios";
 import CardProduct from "../components/CardProduct";
 import "../style/store.css";
@@ -16,7 +16,10 @@ const url: string = "https://6478b240362560649a2e4a2c.mockapi.io/Goods";
 
 function Store() {
   const [goods, setGoods] = React.useState<IItem[]>([]);
+  const [input, setInput] = React.useState<string>("");
+  const [currentData, setCurrentData] = React.useState<IItem[]>([]);
 
+  // Загрузка данных с бека
   useEffect(() => {
     axios
       .get(url)
@@ -26,33 +29,71 @@ function Store() {
       .catch((error) => {
         console.error("Ошибка при получении данных");
       });
-    console.log(goods);
-  });
+  }, [url]);
+
+  // Поиск товаров через инпут
+  const filteredGoods = goods.filter((item) =>
+    item.name.toLowerCase().includes(input.toLowerCase())
+  );
+
+  const selectSort = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value == "all") {
+      setCurrentData(goods);
+    } else if (e.target.value.toLowerCase() == "build") {
+      setCurrentData(goods.filter((e) => e.type == "build"));
+    } else if (e.target.value.toLowerCase() == "weapon") {
+      setCurrentData(goods.filter((e) => e.type == "weapon"));
+    } else if (e.target.value.toLowerCase() == "food") {
+      setCurrentData(goods.filter((e) => e.type == "food"));
+    } else if (e.target.value.toLowerCase() == "transport") {
+      setCurrentData(goods.filter((e) => e.type == "transport"));
+    }
+  };
 
   return (
     <div className="wrapper">
       <div className="store">
         <div className="store-search">
-          <input type="text" className="store-search__input" />
-          <select className="store-search__select" name="" id="">
+          <input
+            type="text"
+            placeholder="Найти"
+            className="store-search__input"
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+          <select
+            className="store-search__select"
+            onChange={selectSort}
+            name=""
+            id=""
+          >
+            <option value="all">Все</option>
             <option value="build">Стройматериалы</option>
-            <option value="gun">Оружие</option>
+            <option value="weapon">Оружие</option>
             <option value="food">Еда</option>
             <option value="transport">Транспорт</option>
           </select>
         </div>
         <div className="store-offers">
-          {goods.map((item) => (
-            <CardProduct
-              id={item.id}
-              image={item.image}
-              name={item.name}
-              quantity={item.quantity}
-              price={item.price}
-              type={item.type}
-              user={item.user}
-            />
-          ))}
+          {currentData
+            .filter((el) => {
+              return input.toLowerCase() === ""
+                ? el
+                : el.name.toLowerCase().includes(input.toLowerCase());
+            })
+            .map((item) => (
+              <CardProduct
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                quantity={item.quantity}
+                price={item.price}
+                type={item.type}
+                user={item.user}
+              />
+            ))}
         </div>
       </div>
     </div>
